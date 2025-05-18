8ab48513b5bc6d082e6f04b56707722568b48a39
N <norberto.tl91@gmail.com>
2025-05-17 21:46:02 -0600

    Update api.ts with env variable logic.

--- src/systems/api.ts
+++ src/systems/api.ts
@@ -1,18 +1,18 @@
 import axios from 'axios';
 
-const ASTRA_ROOT_URL = 'http://127.0.0.1:8000'
+const rootURL = import.meta.env.BACKEND_ROOT || 'http://127.0.0.1:8000';
 
 export const fetchDXInstances = async () => {
-    const response = await axios.get(`${ASTRA_ROOT_URL}/api/v1/dx_instances/`);
+    const response = await axios.get(`${rootURL}/api/v1/dx_instances/`);
     return response.data;
 }
 
 export const fetchPointsOfInterest = async () => {
-    const response = await axios.get(`${ASTRA_ROOT_URL}/api/v1/pois/`);
+    const response = await axios.get(`${rootURL}/api/v1/pois/`);
     return response.data;
 }
 
 export const fetchLatestGrids = async () => {
-    const response = await axios.get(`${ASTRA_ROOT_URL}/api/v1/grid_positions/latest/`);
+    const response = await axios.get(`${rootURL}/api/v1/grid_positions/latest/`);
     return response.data
 }
